bfbec4a23b08668d8cf8b668ea4c3fc908785b2f
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-01-28 11:48:26 +0000

    Calling the function and returing and returning and then calling

--- 6 Recursion/1.py
+++ 6 Recursion/1.py
@@ -3,7 +3,9 @@
 #when a function calls itself, it is called a recursive function
 #Recursion is a common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result.   
 #The developer should be very careful with recursion as it can be quite easy to slip into writing a function which never terminates, or one that uses excess amounts of memory or processor power. However, when written correctly recursion can be a very efficient and mathematically-elegant approach to programming.
+#recurasion has two phase 1. base case 2. recursive case and it has twi types, calling phase and returning phase
 
+# Factorial of a number
 def factorial(n):
     # Base case: if n is 0 or 1, return 1
     if n == 0 or n == 1:

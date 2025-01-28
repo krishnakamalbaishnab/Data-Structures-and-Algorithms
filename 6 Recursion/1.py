b3467cacdc1210c4b647df7da80d520561965727
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-01-28 11:41:48 +0000

    Recursion

---
+++ 6 Recursion/1.py
@@ -0,0 +1,14 @@
+# Difficulty: Easy
+
+
+
+def factorial(n):
+    # Base case: if n is 0 or 1, return 1
+    if n == 0 or n == 1:
+        return 1
+    # Recursive case: n * factorial of (n-1)
+    else:
+        return n * factorial(n - 1)
+
+# Example usage
+print(factorial(6))  # Output: 120

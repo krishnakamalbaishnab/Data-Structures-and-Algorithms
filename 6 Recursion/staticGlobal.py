f356027424dddffaf75b224fa9a2fcc2cfd6452e
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-01-30 10:19:25 +0000

    static variable in recursion

--- 6 Recursion/staticGlobal.py
+++ 6 Recursion/staticGlobal.py
@@ -1,3 +1,5 @@
+#global varibale in recursion
+
 def function(n):
     if n>0:
         return function(n-1)+n
@@ -11,4 +13,22 @@ if __name__ == "__main__":
 
 
     
-    
+#static variable in recursion
+
+
+
+
+
+
+
+def function(n, x=[0]):
+    if n > 0:
+        x[0] += 1
+        return function(n - 1) + x[0]
+    return 0
+
+def main():
+    print(function(5))
+
+if __name__ == "__main__":
+    main()

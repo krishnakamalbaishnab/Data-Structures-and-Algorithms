ec2fbb481788722652cb0491aa82e5c40f4a8d71
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-06 11:45:48 +0000

    Separated CPP  and Python

--- 0 Java/Big O/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public void long {int [] numbers}
-        for (int first : numbers);
-            for (int second: numbers);
-        System.out.println(first " " + second);
-    }
-}

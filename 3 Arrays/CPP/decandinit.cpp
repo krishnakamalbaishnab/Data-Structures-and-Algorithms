0250e337b8ce20b690833d3bc23c729d0992206a
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-06 12:03:19 +0000

    declaring array with garbage value and checking how does they works

---
+++ 3 Arrays/CPP/decandinit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int array1[5] = {};
+    int array2[5] = {1,2,3,4,5};
+    int array3[5] = {2,4};
+    int array4[5] = {0};
+    int array5[] = {1,2,3,4,5};
+
+    cout<<array1<<endl;
+    cout<<array2<<endl;
+    cout<<array3[2]<<endl;
+    cout<<array4<<endl;
+    cout<<array5[4]<<endl;
+
+
+    
+
+
+}

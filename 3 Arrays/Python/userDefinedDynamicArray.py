a7909774e9cf119733a753451f15a36c42c27480
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-10 10:01:23 +0000

    without class

--- 3 Arrays/Python/userDefinedDynamicArray.py
+++ 3 Arrays/Python/userDefinedDynamicArray.py
@@ -22,3 +22,37 @@ if __name__ == "__main__":
     arr = Array(10)
     arr.create()
     arr.display()
+
+
+
+##TODO: nly function based
+
+
+def create_array(size):
+    length = int(input("Enter the number of elements: "))
+    
+    if length > size:
+        print("Error: Length cannot exceed size!")
+        return [], 0  # Return empty array and 0 length
+    
+    arr = []
+    print("Enter the array elements:")
+    for i in range(length):
+        arr.append(int(input(f"Array element [{i}] = ")))
+
+    return arr, length
+
+
+def display_array(arr, length):
+    if length == 0:
+        print("Array is empty.")
+    else:
+        print("Array elements:", " ".join(map(str, arr)))
+
+
+if __name__ == "__main__":
+    size = 10  # Maximum size of the array
+    arr, length = create_array(size)  # Create array and get length
+    display_array(arr, length)  # Display elements
+
+

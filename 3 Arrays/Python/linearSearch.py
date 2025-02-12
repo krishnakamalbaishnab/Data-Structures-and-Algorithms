309b3b71bf6aa4d5ba682cf65f162e399b00e0a1
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-12 10:06:59 +0000

    Linear search

--- 3 Arrays/Python/linearSearch.py
+++ 3 Arrays/Python/linearSearch.py
@@ -1,17 +1,33 @@
+# import array
+
+# arr1 = array.array('i',[1,2,3,5,6,7,8,9,10])
+
+# print(list(arr1))
+
+# #search element 5 i the array
+# key = 7
+
+# for i in range(len(arr1)):
+# 	if key == arr1[i]:
+# 		print ("The element found at index : ", i)
+# 	else:
+# 	print("Element not found!")
+
+
+
+
 import array
 
-arr1 = array.array('i',[1,2,3,5,6,7,8,9,10])
+arr1 = array.array('i', [1, 2, 3, 5, 6, 7, 8, 9, 10])
 
-print(list(arr1))
+key = 11
 
-#search element 5 i the array
-key = 7
+if key in arr1:
+    print("The element found at index:", arr1.index(key))
+else:
+    print("Element not found!")
 
-for i in range(len(arr1)):
-	if key == arr1[i]:
-		print ("The element found at index : ", i)
-	else:
-	print("Element not found!")
+
 
 
 
@@ -36,3 +52,35 @@ for i in range(len(arr1)):
 
 if not found:  # Only print once, after loop
     print("Element not found!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+#using while loop
+
+
+
+import array
+
+arr1 = array.array('i', [1, 2, 3, 5, 6, 7, 8, 9, 10])
+
+key = 11
+i = 0  # Start index
+
+while i < len(arr1):
+    if arr1[i] == key:
+        print("The element found at index:", i)
+        break  # Exit loop when found
+    i += 1
+else:
+    print("Element not found!")  # Runs only if loop completes without `break`
+

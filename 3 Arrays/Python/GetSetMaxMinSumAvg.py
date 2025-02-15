d924e8510de643ed13c7bfd8a035f3e84d5c39b9
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-15 11:31:05 +0000

    Set Elements

--- 3 Arrays/Python/GetSetMaxMinSumAvg.py
+++ 3 Arrays/Python/GetSetMaxMinSumAvg.py
@@ -23,40 +23,26 @@ import array
 
 ##set method
 
-# def setElements(array, index, element):
-# 	if index>0 and index<len(array):
-# 		array[index] = element
-# 	else:
-# 		return IndexError("Index out of range!")
+def setElements(array, index, element):
+	if index>0 and index<len(array):
+		array[index] = element
+		return array
+		return IndexError("Index out of range!")
 
 
-# array1 = array.array('i', [1, 2, 3, 9, 6, 4, 3, 11, 10])
+array1 = array.array('i', [1, 2, 3, 9, 6, 4, 3, 11, 10])
 
+print(array1)
+
+# print(getElement(array1,4, 99))
+
+
+print(setElements(array1,4,99))
 # print(array1)
 
-# # print(getElement(array1,4, 99))
-
-
-# print(setElements(array1,4,99))
-# # print(array1)
 
 
 
-import array
-
-def setElements(arr, index, element):
-    if 0 <= index < len(arr):
-        arr[index] = element
-        return arr  # Return the modified array
-    else:
-        raise IndexError("Index out of range!")
-
-# Initialize array
-arr1 = array.array('i', [1, 2, 3, 9, 6, 4, 3, 11, 10])
-
-# Example usage
-print(setElements(arr1, 4, 99))  
-# Output: array('i', [1, 2, 3, 9, 99, 4, 3, 11, 10])
 
 
 

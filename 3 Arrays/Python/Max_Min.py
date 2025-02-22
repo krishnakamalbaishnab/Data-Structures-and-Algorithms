32b86608d32ce15160bceeb76db6a558d14b7536
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-02-22 10:25:21 +0000

    Minimum in a array

--- 3 Arrays/Python/Max_Min.py
+++ 3 Arrays/Python/Max_Min.py
@@ -1,22 +1,45 @@
 #function to find the maximum of a array
-import array
 
+
+import array
 
 
 def find_max(arr):
 
-	max_value = array1[0]
+	max_value = arr[0]
 
-	for i in range(len(array1)):
+	for i in range(len(arr)):
 		if arr[i] > max_value:
 			max_value = arr[i]
 
 	return max_value
 
 
+
+
+
+#function to find the minimum in a array
+
+
+
+def find_min(arr):
+	min_value = arr[0]
+
+	for i in range(len(arr)):
+		if arr[i] < min_value:
+			min_value = aar[i]
+
+	return min_value
+
+
+
+
+
 array1 = array.array('i', [1,2,6,4,7,9,10])
 
+
 max_value  = find_max(array1)
+min_value = find_min(array1)
+
+print(min_value)
 print(max_value)
-
-

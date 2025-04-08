30c20f263ef49ecb67bccad20199ffacb0d24d01
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-04-08 09:22:09 +0100

    cpp and python code added for declaring a linked list

---
+++ 8 Linked_List/CPP/LL1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+// Node class
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+// Linked List class
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    // Append a node to the end
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    // Print the list
+    void printList() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " -> ";
+            current = current->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+
+
+int main() {
+    LinkedList list;
+
+    list.append(10);
+    list.append(20);
+    list.append(30);
+
+    list.printList();  // Output: 10 -> 20 -> 30 -> NULL
+
+    return 0;
+}

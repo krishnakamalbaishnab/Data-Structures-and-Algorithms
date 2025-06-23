de84fae1c0854233017e4c1ada0092e54c002b15
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-06-23 12:07:40 +0100

    pattern 5

--- TUF+/Patterns/5pattern.cpp
+++ TUF+/Patterns/5pattern.cpp
@@ -1,8 +1,9 @@
 
-// *
-// **
-// ***
+
 // ****
+// ***
+// **
+// *
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,9 +14,9 @@ int main()
     int n;
     cin >> n;
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= n - i + 1; j++)
         {
             cout << "*";
         }

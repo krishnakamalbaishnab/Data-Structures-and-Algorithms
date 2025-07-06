67b431197b5a43799f8252d0951ada08f3e44530
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-07-06 11:34:35 +0100

    Loop thorugh all the elements of vector and print the elements

--- TUF+/C++_STL/2Vectors.cpp
+++ TUF+/C++_STL/2Vectors.cpp
@@ -7,6 +7,7 @@ void Explain_vector()
     vector1.push_back(2);
     vector1.push_back(4);
     vector1.push_back(5);
+    // loop through all the elements of the vector and print it in console
     for (int value : vector1)
     {
         cout << value << " " << endl;

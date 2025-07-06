66c072f5452a4d20a7988000b774d9399746f4c4
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-07-06 11:33:08 +0100

    vectors

---
+++ TUF+/C++_STL/2Vectors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Explain_vector()
+{
+    vector<int> vector1;
+    vector1.push_back(2);
+    vector1.push_back(4);
+    vector1.push_back(5);
+    for (int value : vector1)
+    {
+        cout << value << " " << endl;
+    }
+}
+
+int main()
+{
+    Explain_vector();
+    return 0;
+}

c9357648bc7939e763bcaddc9ab6a339a8838223
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-07-07 10:53:36 +0100

    stack

---
+++ TUF+/C++_STL/6Stack.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void ExplainStack()
+{
+    stack<int> stack1;
+    stack1.push(7);
+    stack1.push(8);
+
+    cout << stack1.top(); // gives tge top most elements
+}
+
+int main()
+{
+
+    ExplainStack();
+
+    return 0;
+}

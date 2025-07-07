82b132fb3196d7f0571f06113c18e5525f139d3b
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-07-07 10:58:59 +0100

    accessing all the elements in stack

--- TUF+/C++_STL/6Stack.cpp
+++ TUF+/C++_STL/6Stack.cpp
@@ -6,8 +6,24 @@ void ExplainStack()
     stack<int> stack1;
     stack1.push(7);
     stack1.push(8);
+    stack1.push(9);
+    stack1.push(5);
+    stack1.push(3);
+    stack1.push(0);
 
-    cout << stack1.top(); // gives tge top most elements
+    // stack<string> stack2;
+    // stack2.push("K");
+    // stack2.push("B");
+
+    // cout << stack1.top(); // gives tge top most elements
+
+    // accessing all the elements of a stack
+
+    while (stack1.empty() == false)
+    {
+        cout << stack1.top() << " ";
+        stack1.pop();
+    }
 }
 
 int main()
